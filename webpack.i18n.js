acb6398ec1aca7fb9946943c33048142b72411c7
Tomasz Kępa <tomaszkepa10@gmail.com>
2020-11-16 20:39:01 +0100

    FEAT: Added Internationalization

---
+++ webpack.i18n.js
@@ -0,0 +1,51 @@
+/*
+const path = require("path");
+const I18nPlugin = require("embed-i18n-webpack-plugin");
+
+const TRANSLATIONS = {
+    pl: { greeting: { hello: "Witaj Swiecie" } },
+    en: { greeting: { hello: "Hello World" } },
+};
+
+module.exports = Object.entries(TRANSLATIONS).map(
+    ([language, translation]) => ({
+        mode: "production",
+        entry: { index: path.join(__dirname, "src", "i18n.js") },
+        output: {
+            path: path.join(__dirname, "i18n-build"),
+            filename: `[name].${language}.js`,
+        },
+        plugins: [new I18nPlugin(translation)],
+    })
+)
+*/
+const path = require("path");
+const I18nPlugin = require("embed-i18n-webpack-plugin");
+
+const TRANSLATIONS = {
+  fi: {
+    greeting: {
+      hello: "Terve maailma",
+    },
+  },
+  en: {
+    greeting: {
+      hello: "Hello world",
+    },
+  },
+};
+
+module.exports = Object.entries(TRANSLATIONS).map(
+  ([language, translation]) => ({
+    mode: "production",
+    entry: {
+      index: path.join(__dirname, "src", "i18n.js"),
+    },
+    output: {
+      path: path.join(__dirname, "i18n-build"),
+      filename: `[name].${language}.js`,
+    },
+    plugins: [new I18nPlugin(translation)
+    ],
+  })
+);
